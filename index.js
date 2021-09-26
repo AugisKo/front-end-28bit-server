ad24887f2b42b6199f36cf785d06af52995987f4
AugisKo <82829963+AugisKo@users.noreply.github.com>
2021-09-26 11:04:36 +0300

    MySQL

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const app = {}
 
 app.init = () => {
     // paruosti reikiamus direktorijas
-
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'barsukas',
+    });
+    
+    // paruosti reikiamus direktorijas
+    
     // paruosti reikiamus failus
 
     // inicijuojame serveri
-    server.init();
+    server.init(connection);
 }
 
 app.init();
